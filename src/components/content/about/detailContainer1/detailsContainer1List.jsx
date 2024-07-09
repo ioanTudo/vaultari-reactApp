31a5bc78721ab925811e2fc0767c507b995ff23b
ioan-tudo <tuddorionut63@yahoo.com>
2024-07-09 12:53:00 +0300

    contact-page and about-page fully responsive on 500, 750 and 1200px

--- src/components/content/about/detailContainer1/detailsContainer1List.jsx
+++ src/components/content/about/detailContainer1/detailsContainer1List.jsx
@@ -4,7 +4,10 @@ import styles from "./detailsContainer1.module.css";
 export const DetailContainer1List = () => {
   return detailsContainer1Data.map((dataContainer1) => (
     <div className={styles.detailContainer1GridWrapper} key={dataContainer1.id}>
-      <div className={styles.detailContainer1TextContainer}>
+      <div
+        className={styles.detailContainer1TextContainer}
+        key={dataContainer1.id}
+      >
         <h1 className={styles.headingDetailContainer1}>
           {dataContainer1.heading}
         </h1>
--- src/components/content/about/detailsContainer3/detailsContainer3List.jsx
+++ src/components/content/about/detailsContainer3/detailsContainer3List.jsx
@@ -5,7 +5,10 @@ export const DetailsContainer3List = () => {
   return detailsContainer3Data.map((dataContainer3) => (
     <div className={styles.detailContainer3GridWrapper} key={dataContainer3.id}>
       <img src={dataContainer3.source} alt={dataContainer3.alt} />
-      <div className={styles.detailContainer3TextContainer}>
+      <div
+        className={styles.detailContainer3TextContainer}
+        key={dataContainer3.id}
+      >
         <h1 className={styles.headingDetailContainer3}>
           {dataContainer3.heading}
         </h1>
